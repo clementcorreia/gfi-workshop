e310c44843f5e4eab911e135c52c92851f7ad58b
Clément Correia <clement.correia34@gmail.com>
2017-10-03 16:37:38 +0200

    Correction sécurité pages accessibles par connexion

--- controller/routeur.php
+++ controller/routeur.php
@@ -5,33 +5,37 @@
 
 $controllerParDefaut = "";
 
-if (isset($_GET['controller'])) {
-	$controller = htmlspecialchars($_GET['controller']);
-}
-else {
-	$controller = "$controllerParDefaut";
-}
-
-$controller_class = 'Controller'.ucfirst($controller);
-
-if (class_exists($controller_class)) {
-	require_once File::build_path(array('controller', $controller_class.'.php'));
-	if (isset($_GET['action'])) {
-	    $action = htmlspecialchars($_GET['action']);    // On recupère l'action passée dans l'URL
-	    if (in_array($action, get_class_methods($controller_class))) {
-	        $controller_class::$action(); // Appel de la méthode statique $action du Controller appelé
-	    }
-	    else {
-	        $controller_class::readAll();
-	    }
+if (isset($_SESSION['user'])) {
+	if (isset($_GET['controller'])) {
+		$controller = htmlspecialchars($_GET['controller']);
 	}
 	else {
-	    $controller_class::readAll();
+		$controller = "$controllerParDefaut";
+	}
+
+	$controller_class = 'Controller'.ucfirst($controller);
+
+	if (class_exists($controller_class)) {
+		require_once File::build_path(array('controller', $controller_class.'.php'));
+		if (isset($_GET['action'])) {
+		    $action = htmlspecialchars($_GET['action']);    // On recupère l'action passée dans l'URL
+		    if (in_array($action, get_class_methods($controller_class))) {
+		        $controller_class::$action(); // Appel de la méthode statique $action du Controller appelé
+		    }
+		    else {
+		        $controller_class::readAll();
+		    }
+		}
+		else {
+		    $controller_class::readAll();
+		}
+	}
+	else {
+		Controller::error();
 	}
 }
 else {
-	require_once File::build_path(array('controller','Controller.php'));
-	Controller::error();
+	Controller::readAll();
 }
 
 ?>
